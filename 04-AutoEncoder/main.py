39ca28e1e545b50598cfdbd87968357354a8183b
CYC <yenchen0416@gmail.com>
2018-08-07 23:36:45 +0800

    add reconstruct training images

--- 04-AutoEncoder/main.py
+++ 04-AutoEncoder/main.py
@@ -12,25 +12,41 @@ strOutputFolderPath = os.path.join(strProjectFolderPath, "02-Output")
 
 # prepare data
 ETL = DataProcessing.prepareData()
-ETL.cleanData(strDataFileName="train.csv", boolLabel=True)
-ETL.doNormalize()
+ETL.cleanData(strDataFileName="train.csv", boolLabel=True, boolNormal=True)
 mnist = ETL.dictData["Data"]
 
 # hyperparameter
 n_input = 784
-folat_Learning_rate = 0.01
-int_Epochs = 100
+n_hidden = [256, 64, 2]
+folat_Learning_rate = 0.0005
+int_Epochs = 150
 int_Batch_size = 200
 
 # model
 AutoEncoder = Model.AutoEncoder(n_input=n_input
-                              , float_Learning_rate=folat_Learning_rate
-                              , n_hidden=[256, 128, 2])
+                              , n_hidden=n_hidden
+                              , float_Learning_rate=folat_Learning_rate)
 
 # training
 list_Loss = AutoEncoder.fit(X=mnist
+                          , Y=mnist
                           , int_Epochs=int_Epochs
                           , int_Batch_size=int_Batch_size)
 
 plt.plot(np.arange(len(list_Loss)), list_Loss)
 plt.savefig(os.path.join(strOutputFolderPath, "LossCurve_lr={}.jpg").format(folat_Learning_rate))
+
+AutoEncoder.save(path=os.path.join(strOutputFolderPath, "AutoEncoder"))
+
+AutoEncoder.reload(path=os.path.join(strOutputFolderPath, "AutoEncoder"))
+
+# reconstruct images
+fig, axis = plt.subplots(2, 10, figsize=(10, 2))
+for i in range(10):
+    img_train_org = mnist[i].reshape(28, 28)
+    axis[0][i].imshow(img_train_org, cmap="gray")
+
+    img_train = np.reshape(AutoEncoder.predict(mnist[i].reshape(-1, 784)), (28, 28))
+    axis[1][i].imshow(img_train, cmap="gray")
+plt.savefig(os.path.join(strOutputFolderPath, "recons_Train.jpg"))
+
